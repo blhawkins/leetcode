cb361eb7f06ee7ad0e60fa02521b53771d8b3271
Benjamin Hawkins <66797762+blhawkins@users.noreply.github.com>
2021-02-17 14:46:44 -0600

    Create combine_two_tables.sql

---
+++ SQL/combine_two_tables.sql
@@ -0,0 +1,15 @@
+#For the following table schema, write a SQL query for a repost that provides the following information for each person in the Person table, 
+#regardless if there is an address for each of these people
+
+#Create table Person (PersonId int, FirstName varchar(255), LastName varchar(255))
+#Create table Address (AddressId int, PersonId int, City varchar(255), State varchar(255))
+#Truncate table Person
+#insert into Person (PersonId, LastName, FirstName) values ('1', 'Wang', 'Allen')
+#Truncate table Address
+#insert into Address (AddressId, PersonId, City, State) values ('1', '2', 'New York City', 'New York')
+
+#Submission
+select p.FirstName, p.LastName, a.City, a.State
+from Person p
+    left join Address a
+        on (p.PersonID = a.PersonID)
