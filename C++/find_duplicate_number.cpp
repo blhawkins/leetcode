e9d7fc132d542b0b17ae9828c23f6bf72ed6ae17
Benjamin Hawkins <66797762+blhawkins@users.noreply.github.com>
2021-02-17 22:58:13 -0600

    Update find_duplicate_number.cpp

--- C++/find_duplicate_number.cpp
+++ C++/find_duplicate_number.cpp
@@ -1,5 +1,8 @@
-//Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+//Leetcode Algorithm #287
+//Difficulty: Medium
 
+//Instructions
+//Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
 //There is only one repeated number in nums, return this repeated number.
 
 class Solution {
